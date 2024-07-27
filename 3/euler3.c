6b91e20371d24a1e80a0bb05ef0c0ca0fec6857f
Tommie Marshall <55310447+tomarshall@users.noreply.github.com>
2024-07-27 02:09:07 +0100

    Completed question 3

--- 3/euler3.c
+++ 3/euler3.c
@@ -1,27 +1,33 @@
-#include <stlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-long largest_prime(long N);
+typedef unsigned long long llu;
+
+llu largest_prime(llu N);
 
 int main(int argc, char* argv[])
 {
     /* Enter number you want to find largest prime factor of */
-    const long N = atoi(argv[1]);
+    //llu N = atoi(argv[1]);
+    llu N = 600851475143;
 
-    printf("The largest prime in %li is %li", N, largest_prime(N)); 
+    printf("The largest prime in %llu is %llu\n", N, largest_prime(N)); 
 }
 
-long largest_prime(long N)
+llu largest_prime(llu N)
 {
-    long p = 0;
+    llu p = -1;
+    llu divisor = 2;
 
-    for (int i = (N / 2); i > 1; i--)
+    while (N > 1)
     {
-        if (N % i == 0)
+        while (N % divisor == 0)
         {
-            
+            N = N / divisor;
+            p = divisor;
         }
+        divisor++;
     }
 
-    return 
+    return p;
 }
